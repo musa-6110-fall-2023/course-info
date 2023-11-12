9f24dc28d0d80ab520192e62d902fc2dc5fd1fe5
Mjumbe Poe <mjumbewu@gmail.com>
2023-11-12 15:00:32 -0500

    Update week11 in-class code

--- week11/handling-dynamic-errors/js/main.js
+++ week11/handling-dynamic-errors/js/main.js
@@ -1,7 +1,7 @@
 const mapboxApiKey = 'pk.eyJ1IjoibWp1bWJlLXRlc3QiLCJhIjoiY2w3ZTh1NTIxMTgxNTQwcGhmODU2NW5kaSJ9.pBPd19nWO-Gt-vTf1pOHBA';
 
 function initMap() {
-  const map = L.map('map', {preferCanvas: true, zoomSnap: 0, zoomDelta: 1}).setView([39.99, -75.15], 16);
+  const map = L.map('map', {zoomSnap: 0, zoomDelta: 1}).setView([39.99, -75.15], 16);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{username}/{styleId}/tiles/{tileSize}/{z}/{x}/{y}{r}?access_token={apiKey}', {
     username: 'mapbox',
@@ -15,17 +15,77 @@ function initMap() {
   }).addTo(map);
 
   map.currentLocationLayer = L.layerGroup().addTo(map);
-  map.routeLayer = L.layerGroup().addTo(map);
+  map.routeLayer = L.geoJSON(null, {
+    style: {
+      // More options in the Path Options on Leaflet docs
+      // https://leafletjs.com/reference.html#path-option
+      color: 'gray',
+      dashArray: '10 10',
+    },
+  }).addTo(map);
+
+  let currentLocation = null;
 
   navigator.geolocation.watchPosition(
       (pos) => {
         map.currentLocationLayer.clearLayers();
-        const currentLocation = [pos.coords.latitude, pos.coords.longitude];
+
+        currentLocation = [pos.coords.latitude, pos.coords.longitude];
         const currentLocationMarker = L.circleMarker(currentLocation);
+
         map.currentLocationLayer.addLayer(currentLocationMarker);
         map.panTo(currentLocation);
       },
+      (err) => {
+        if (err.code === err.PERMISSION_DENIED) {
+          alert('Sorry, I can\'t give you directions if you don\'t give me your location');
+        } else if (err.code === err.POSITION_UNAVAILABLE) {
+          alert('Sorry, I wasn\'t able to determine your position.');
+        } else {
+          alert(`There was an error that I'm unfamiliar with: ${err.code}`);
+        }
+      },
   );
+
+  map.addEventListener('click', async (evt) => {
+    const fromLatLng = currentLocation;
+    const toLatLng = evt.latlng;
+
+    const url = `https://api.mapbox.com/directions/v5/mapbox/walking/${fromLatLng[1]},${fromLatLng[0]};${toLatLng.lng},${toLatLng.lat}` +
+      `?geometries=geojson` +
+      `&access_token=${mapboxApiKey}`;
+
+    // const resp = await fetch(url);
+    // const data = await resp.json();
+
+    let resp;
+    let data;
+    try {
+      resp = await fetch(url);
+
+      if (resp.status !== 200) {
+        throw new Error('Something exceptional happened.');
+      }
+
+      data = await resp.json();
+    } catch (err) {
+      alert('Hey, something happend. Sorry.');
+      return;
+    }
+
+    console.log(data);
+
+    // The response will have one or more routes. 🤔 Maybe there can be 0, so
+    // we'll handle that case as well.
+    if (data.routes.length < 1) {
+      alert('Looks like there are no routes to there from where you are.');
+    }
+
+    const routeGeom = data.routes[0].geometry;
+    map.routeLayer.clearLayers();
+    map.routeLayer.addData(routeGeom);
+    map.routeLayer.addLayer(L.marker(toLatLng));
+  });
 
   return map;
 }
